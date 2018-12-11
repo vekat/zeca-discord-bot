4af3df4910cde17f2157ae67f0573a0ab2fc045c
Vitor Cortez <vitor.a.cortez@gmail.com>
2018-12-10 21:42:58 -0300

    Add `getSettings` helper function

--- util.js
+++ util.js
@@ -1,10 +1,12 @@
+const util = exports
+
 /**
  * Fetches the permission level from a member's message.
  * @param {Message} message discord message
  * @param {object} settings guild settings
  * @returns Permission level
  */
-exports.getPermission = function getPermission (message, settings) {
+util.getPermission = function getPermission (message, settings) {
   if (message.author.id === message.guild.ownerID) return 4
   if (message.member.roles.some((r) => settings.administratorRoles.includes(r.name))) return 3
   if (message.member.roles.some((r) => settings.moderatorRoles.includes(r.name))) return 2
@@ -13,8 +15,25 @@ exports.getPermission = function getPermission (message, settings) {
 }
 
 /**
+ * Fetches the settings for a given guild.
+ * @param {Client} client
+ * @param {number} guildId
+ * @returns {object} Settings object
+ */
+util.getSettings = function getSettings (client, guildId) {
+  if (!client.settings.has(guildId)) {
+    client.settings.set(guildId, {})
+  }
+
+  const { defaults } = client.config
+  const guildOverrides = client.settings.get(guildId)
+
+  return { ...defaults, ...guildOverrides }
+}
+
+/**
  * Async timeout.
  */
-exports.timeout = function timeout (ms) {
+util.timeout = function timeout (ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
